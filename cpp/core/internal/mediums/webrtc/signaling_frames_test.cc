4c43ad4e6cb30820019f15c8b538e212f4a62379
hai007 <hais@google.com>
2021-03-23 10:33:56 -0700

    Internal change
    PiperOrigin-RevId: 364590475

--- cpp/core/internal/mediums/webrtc/signaling_frames_test.cc
+++ cpp/core/internal/mediums/webrtc/signaling_frames_test.cc
@@ -86,11 +86,11 @@ TEST(SignalingFramesTest, SignalingPoke) {
   location::nearby::mediums::WebRtcSignalingFrame frame;
   frame.ParseFromString(std::string(encoded_poke.data(), encoded_poke.size()));
 
-  EXPECT_THAT(frame, testing::EqualsProto(R"(
+  EXPECT_THAT(frame, testing::EqualsProto(R"pb(
                 sender_id { id: "abc" }
                 type: READY_FOR_SIGNALING_POKE_TYPE
                 ready_for_signaling_poke {}
-              )"));
+              )pb"));
 }
 
 TEST(SignalingFramesTest, EncodeValidOffer) {
