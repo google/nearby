258403118d3bd98ec6c52eeb6db0d626af05249d
hai007 <hais@google.com>
2021-05-26 12:29:43 -0700

    [Nearby] Directly include base/logging.h and copybara transform to base/check.h in chromium
    PiperOrigin-RevId: 376003869

--- cpp/platform/base/logging.h
+++ cpp/platform/base/logging.h
@@ -15,6 +15,9 @@
 #ifndef PLATFORM_BASE_LOGGING_H_
 #define PLATFORM_BASE_LOGGING_H_
 
+// base/logging.h is only included to allow logging clients to include CHECK's.
+// In Chrome this will be translated to base/check.h. See crbug/1212611.
+#include "base/check.h"
 #include "platform/api/log_message.h"
 #include "platform/api/platform.h"
 
