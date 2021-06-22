2e853156dc1a2d38247e7ac780fd354ea24762e6
hai007 <hais@google.com>
2021-06-22 11:08:10 -0700

    Internal change
    PiperOrigin-RevId: 380848866

---
+++ cpp/platform/impl/windows/generated/sources/BluetoothClassicMedium.cpp
@@ -0,0 +1,24 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pch.h"
+#include "BluetoothClassicMedium.h"
+
+// Note: Remove this static_assert after copying these generated source files to your project.
+// This assertion exists to avoid compiling these generated source files directly.
+static_assert(false, "Do not compile generated C++/WinRT source files directly");
+
+namespace winrt::location::nearby::windows::implementation
+{
+}
