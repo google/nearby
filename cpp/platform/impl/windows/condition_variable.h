da389248eddaf95371c06192bb243f39b26dd050
jfcarroll <jfcarroll@google.com>
2021-10-04 13:08:20 -0700

    Symbol Export for UI.
    PiperOrigin-RevId: 400796234

--- cpp/platform/impl/windows/condition_variable.h
+++ cpp/platform/impl/windows/condition_variable.h
@@ -11,58 +11,45 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #ifndef PLATFORM_IMPL_WINDOWS_CONDITION_VARIABLE_H_
 #define PLATFORM_IMPL_WINDOWS_CONDITION_VARIABLE_H_
-
-#include <windows.h>
 #include <stdio.h>
 #include <synchapi.h>
+#include <windows.h>
 
 #include <mutex>  //  NOLINT
 
 #include "platform/api/condition_variable.h"
 #include "platform/impl/windows/mutex.h"
+#include "platform/public/mutex.h"
 
 namespace location {
 namespace nearby {
 namespace windows {
 
-// The ConditionVariable class is a synchronization primitive that can be used
-// to block a thread, or multiple threads at the same time, until another thread
-// both modifies a shared variable (the condition), and notifies the
-// ConditionVariable.
 class ConditionVariable : public api::ConditionVariable {
  public:
-  ConditionVariable(api::Mutex* mutex);
-
+  explicit ConditionVariable(api::Mutex* mutex)
+      : mutex_(&(static_cast<windows::Mutex*>(mutex))->mutex_) {}
   ~ConditionVariable() override = default;
 
-  // Notifies all the waiters that condition state has changed.
-  void Notify() override;
+  Exception Wait() override {
+    cond_var_.Wait(mutex_);
+    return {Exception::kSuccess};
+  }
 
-  // Waits indefinitely for Notify to be called.
-  // May return prematurely in case of interrupt, if supported by platform.
-  // Returns kSuccess, or kInterrupted on interrupt.
-  Exception Wait() override;
+  Exception Wait(absl::Duration timeout) override {
+    cond_var_.WaitWithTimeout(mutex_, timeout);
+    return {Exception::kSuccess};
+  }
 
-  // Waits while timeout has not expired for Notify to be called.
-  // May return prematurely in case of interrupt, if supported by platform.
-  // Returns kSuccess, or kInterrupted on interrupt.
-  Exception Wait(absl::Duration timeout) override;
+  void Notify() override { cond_var_.SignalAll(); }
 
  private:
-  location::nearby::windows::Mutex& mutex_;
-
-  std::condition_variable condition_variable_actual_;
-  std::condition_variable& condition_variable_ = condition_variable_actual_;
-
-  std::unique_lock<std::mutex> lock;
-  CRITICAL_SECTION critical_section_;
+  absl::Mutex* mutex_;
+  absl::CondVar cond_var_;
 };
-
 }  // namespace windows
 }  // namespace nearby
 }  // namespace location
-
 #endif  // PLATFORM_IMPL_WINDOWS_CONDITION_VARIABLE_H_
