240973ede6e5dd99cbb796ab009cc3c0751a4035
jfcarroll <jfcarroll@google.com>
2021-10-04 13:08:19 -0700

    ConditionVariable, CountdownLatch and Mutex changes, using modified g3 versions.
    PiperOrigin-RevId: 400796228

--- cpp/platform/impl/windows/platform.cc
+++ cpp/platform/impl/windows/platform.cc
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #include "platform/api/platform.h"
 
+#include "platform/impl/shared/count_down_latch.h"
 #include "platform/impl/shared/file.h"
 #include "platform/impl/windows/atomic_boolean.h"
 #include "platform/impl/windows/atomic_reference.h"
@@ -22,7 +22,6 @@
 #include "platform/impl/windows/bluetooth_classic_medium.h"
 #include "platform/impl/windows/cancelable.h"
 #include "platform/impl/windows/condition_variable.h"
-#include "platform/impl/shared/count_down_latch.h"
 #include "platform/impl/windows/executor.h"
 #include "platform/impl/windows/future.h"
 #include "platform/impl/windows/listenable_future.h"
@@ -35,120 +34,96 @@
 #include "platform/impl/windows/webrtc.h"
 #include "platform/impl/windows/wifi.h"
 #include "platform/impl/windows/wifi_lan.h"
-
 namespace location {
 namespace nearby {
 namespace api {
 namespace {
-
 std::string GetPayloadPath(PayloadId payload_id) {
   return absl::StrCat("/tmp/", payload_id);
 }
 }  // namespace
-
 std::unique_ptr<AtomicBoolean> ImplementationPlatform::CreateAtomicBoolean(
     bool initial_value) {
   return absl::make_unique<windows::AtomicBoolean>();
 }
-
 std::unique_ptr<AtomicUint32> ImplementationPlatform::CreateAtomicUint32(
     std::uint32_t value) {
   return absl::make_unique<windows::AtomicUint32>();
 }
-
 std::unique_ptr<CountDownLatch> ImplementationPlatform::CreateCountDownLatch(
     std::int32_t count) {
   return absl::make_unique<shared::CountDownLatch>(count);
 }
-
 std::unique_ptr<Mutex> ImplementationPlatform::CreateMutex(Mutex::Mode mode) {
   return absl::make_unique<windows::Mutex>(mode);
 }
-
 std::unique_ptr<ConditionVariable>
 ImplementationPlatform::CreateConditionVariable(Mutex* mutex) {
   return absl::make_unique<windows::ConditionVariable>(mutex);
 }
-
 std::unique_ptr<InputFile> ImplementationPlatform::CreateInputFile(
     PayloadId payload_id, std::int64_t total_size) {
-  return absl::make_unique<shared::InputFile>(
-      GetPayloadPath(payload_id), total_size);
+  return absl::make_unique<shared::InputFile>(GetPayloadPath(payload_id),
+                                              total_size);
 }
-
 std::unique_ptr<OutputFile> ImplementationPlatform::CreateOutputFile(
     PayloadId payload_id) {
-  return absl::make_unique<shared::OutputFile>(
-      GetPayloadPath(payload_id));
+  return absl::make_unique<shared::OutputFile>(GetPayloadPath(payload_id));
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<LogMessage> ImplementationPlatform::CreateLogMessage(
     const char* file, int line, LogMessage::Severity severity) {
   return absl::make_unique<windows::LogMessage>(file, line, severity);
 }
-
 std::unique_ptr<SubmittableExecutor>
 ImplementationPlatform::CreateSingleThreadExecutor() {
   return absl::make_unique<windows::SubmittableExecutor>();
 }
-
 std::unique_ptr<SubmittableExecutor>
 ImplementationPlatform::CreateMultiThreadExecutor(
     std::int32_t max_concurrency) {
   return absl::make_unique<windows::SubmittableExecutor>(max_concurrency);
 }
-
 std::unique_ptr<ScheduledExecutor>
 ImplementationPlatform::CreateScheduledExecutor() {
   return absl::make_unique<windows::ScheduledExecutor>();
 }
-
 std::unique_ptr<BluetoothAdapter>
 ImplementationPlatform::CreateBluetoothAdapter() {
   return absl::make_unique<windows::BluetoothAdapter>();
 }
-
 std::unique_ptr<BluetoothClassicMedium>
 ImplementationPlatform::CreateBluetoothClassicMedium(
     nearby::api::BluetoothAdapter& adapter) {
-  return absl::make_unique<windows::BluetoothClassicMedium>(
-      adapter);
+  return absl::make_unique<windows::BluetoothClassicMedium>(adapter);
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<BleMedium> ImplementationPlatform::CreateBleMedium(
     BluetoothAdapter& adapter) {
   return absl::make_unique<windows::BleMedium>();
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<ble_v2::BleMedium> ImplementationPlatform::CreateBleV2Medium(
     BluetoothAdapter&) {
   return nullptr;
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<ServerSyncMedium>
 ImplementationPlatform::CreateServerSyncMedium() {
   return std::unique_ptr<windows::ServerSyncMedium>();
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<WifiMedium> ImplementationPlatform::CreateWifiMedium() {
   return std::unique_ptr<WifiMedium>();
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<WifiLanMedium> ImplementationPlatform::CreateWifiLanMedium() {
   return absl::make_unique<windows::WifiLanMedium>();
 }
-
 // TODO(b/184975123): replace with real implementation.
 std::unique_ptr<WebRtcMedium> ImplementationPlatform::CreateWebRtcMedium() {
   return absl::make_unique<windows::WebRtcMedium>();
 }
-
 }  // namespace api
 }  // namespace nearby
 }  // namespace location
