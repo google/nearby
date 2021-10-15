d7f0f1b63545e6f42986806e733e3ebdb5a75dbf
jfcarroll <jfcarroll@google.com>
2021-10-15 15:54:23 -0700

    Separation of ConnectionOptions into a header and an implementation file.
    PiperOrigin-RevId: 403486468

--- cpp/core/options.h
+++ cpp/core/options.h
@@ -103,66 +103,26 @@ struct DLL_API ConnectionOptions {
   int keep_alive_timeout_millis = 0;
 
   // Verify if  ConnectionOptions is in a not-initialized (Empty) state.
-  bool Empty() const { return strategy.IsNone(); }
+  bool Empty() const;
 
   // Bring  ConnectionOptions to a not-initialized (Empty) state.
-  void Clear() { strategy.Clear(); }
+  void Clear();
 
   // Returns a copy and normalizes allowed mediums:
   // (1) If is_out_of_band_connection is true, verifies that there is only one
   //     medium allowed, defaulting to only Bluetooth if unspecified.
   // (2) If no mediums are allowed, allow all mediums.
-  ConnectionOptions CompatibleOptions() const {
-    ConnectionOptions result = *this;
+  ConnectionOptions CompatibleOptions() const;
 
-    // Out-of-band connections initiate connections via an injected endpoint
-    // rather than through the normal discovery flow. These types of connections
-    // can only be injected via a single medium.
-    if (is_out_of_band_connection) {
-      int num_enabled = result.allowed.Count(true);
-
-      // Default to allow only Bluetooth if no single medium is specified.
-      if (num_enabled != 1) {
-        result.allowed.SetAll(false);
-        result.allowed.bluetooth = true;
-      }
-      return result;
-    }
-
-    // Normal connections (i.e., not out-of-band) connections can specify
-    // multiple mediums. If none are specified, default to allowing all mediums.
-    if (!allowed.Any(true)) result.allowed.SetAll(true);
-    return result;
-  }
-
-  std::vector<Medium> GetMediums() const { return allowed.GetMediums(true); }
+  std::vector<Medium> GetMediums() const;
 
   // This call follows the standard Microsoft calling pattern of calling first
   // to get the size of the array. Caller then allocates memory for the array,
   // and makes this call again to copy the array into the provided location.
   void GetMediums(location::nearby::proto::connections::Medium* mediums,
-                  uint32_t* mediumsSize) {
-    auto size = GetMediums().size();
-
-    // Caller is seeking the size of mediums
-    if (mediums == nullptr) {
-      *mediumsSize = size;
-      return;
-    }
-
-    // Caller has not specified enough memory
-    if (*mediumsSize < size) {
-      mediums = nullptr;    // ensure nullptr return
-      *mediumsSize = size;  // update the size to indicate the correct size
-      return;
-    }
-
-    for (uint32_t i = 0; i < size; i++) {
-      // Construct an array at the given location
-      mediums[i] = GetMediums().at(i);
-    }
-  }
+                  uint32_t* mediumsSize);
 };
+
 
 // Metadata injected to facilitate out-of-band connections. The medium field is
 // required, and the other fields are only specified for a specific medium.
