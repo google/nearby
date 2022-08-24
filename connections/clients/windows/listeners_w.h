f3c1a76723aa0c0123701e763db23c5db783a30a
John Carroll <jfcarroll@google.com>
2022-08-24 10:05:35 -0700

    this change adds RejectConnectionDart and supports cl/467032081.
    PiperOrigin-RevId: 469757197

--- connections/clients/windows/listeners_w.h
+++ connections/clients/windows/listeners_w.h
@@ -117,7 +117,6 @@ struct DLL_API ConnectionResponseInfoW {
 struct DLL_API PayloadProgressInfoW {
   PayloadId payload_id = 0;
   enum class Status {
-    kUnspecified = 0,
     kSuccess,
     kFailure,
     kInProgress,
