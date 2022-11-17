00fd273eff20dfa89dad69743cc11855357ce0f3
Nick Bourdakos <bourdakos@google.com>
2022-11-17 08:39:42 -0800

    Set swift package platform versions
    PiperOrigin-RevId: 489220091

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
   name: "NearbyConnections",
+  platforms: [
+    .iOS(.v11),
+    .macOS(.v10_13),
+    .tvOS(.v11),
+    .watchOS(.v4),
+  ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
