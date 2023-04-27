b5131f095d94d50c6113ca85ad033604895ce935
Suet-fei Li <suetfei@google.com>
2023-04-27 23:18:28 +0000

    .

--- fastpair/rust/src/types/packets.rs
+++ fastpair/rust/src/types/packets.rs
@@ -1,13 +1,31 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+// https://source.corp.google.com/piper///depot/google3/third_party/nearby/fastpair/message_stream/message.h
+
+#[derive(Debug)]
 pub enum BluetoothCode {
     EnableSilenceMode = 0x01,
     DisableSilenceMode = 0x02,
 }
 
+#[derive(Debug)]
 pub enum CompanionAppEventCode {
     LogBufferFull = 0x01,
 }
 
+#[derive(Debug)]
 pub enum DeviceInformationEventCode {
     ModelId = 0x01,
     BleAddressUpdated = 0x02,
@@ -20,6 +38,7 @@ pub enum DeviceInformationEventCode {
     SessionNonce = 0x0A,
 }
 
+#[derive(Debug)]
 pub enum SassCode {
     Acknowledgement = 0xFF,
     SassGetCapability = 0x10,
@@ -39,15 +58,18 @@ pub enum SassCode {
     SassSetDropConnectionTarget = 0x43,
 }
 
+#[derive(Debug)]
 pub enum DeviceActionEventCode {
     Ring = 1,
 }
 
+#[derive(Debug)]
 pub enum AcknowledgementCode {
     Ack = 1,
     Nack = 2,
 }
 
+#[derive(Debug)]
 pub enum MessageGroup {
     Bluetooth(BluetoothCode),
     CompanionAppEvent(CompanionAppEventCode),
@@ -55,4 +77,11 @@ pub enum MessageGroup {
     DeviceActionEvent(DeviceActionEventCode),
     Sass(SassCode),
     Acknowledgement(AcknowledgementCode),
+}
+
+
+/// A packet that is sent over the RFCOMM Message Stream.
+pub struct MessageStreamPacket {
+    pub group: MessageGroup,
+    pub additional_data: Vec<u8>,
 }
