f4cd4edb60b1fc393b1b8f3a91f95a379d92cc6f
SpooHEX/ <108245326+Spoety40@users.noreply.github.com>
2023-06-30 19:57:05 -0400

    Update nearby_fp_client.h

--- embedded/client/source/nearby_fp_client.h
+++ embedded/client/source/nearby_fp_client.h
@@ -36,61 +36,18 @@ typedef struct {
 // Returns current information on a given seeker.
 typedef struct {
   uint64_t peer_address;   // address of peer/seeker
-  uint8_t capabilities;    // Capability bits
-                           // Bit 0: Companion app is installed/not installed.
+  on app is installed/not installed.
                            // Bit 1: Silence mode is supported/not supported.
   uint8_t platform_type;   // platform type code (Android = 0x01)
   uint8_t platform_build;  // Platform build number (Android Pie=0x1c)
 } nearby_fp_client_SeekerInfo;
 
-// No advertisement, default state
-#define NEARBY_FP_ADVERTISEMENT_NONE 0x00
-// The device is discoverable and can be paired with
-#define NEARBY_FP_ADVERTISEMENT_DISCOVERABLE 0x01
-// The device is not discoverable
-#define NEARBY_FP_ADVERTISEMENT_NON_DISCOVERABLE 0x02
-// Ask the Seeker to show pairing UI indication. This flag can be combined with
-// NEARBY_FP_ADVERTISEMENT_NON_DISCOVERABLE
-#define NEARBY_FP_ADVERTISEMENT_PAIRING_UI_INDICATOR 0x04
-#if NEARBY_FP_ENABLE_BATTERY_NOTIFICATION
-// Include battery and charging info in the advertisement. This flag can be
-// combined with NEARBY_FP_ADVERTISEMENT_NON_DISCOVERABLE
-#define NEARBY_FP_ADVERTISEMENT_INCLUDE_BATTERY_INFO 0x08
-// Ask the Seeker to show Battery UI indication. This flag can be combined with
-// NEARBY_FP_ADVERTISEMENT_INCLUDE_BATTERY_INFO
-#define NEARBY_FP_ADVERTISEMENT_BATTERY_UI_INDICATOR 0x10
-#endif /* NEARBY_FP_ENABLE_BATTERY_NOTIFICATION */
-#ifdef NEARBY_FP_ENABLE_SASS
+
 // Include SASS advertisement. This flag can be
 // combined with NEARBY_FP_ADVERTISEMENT_NON_DISCOVERABLE
 #define NEARBY_FP_ADVERTISEMENT_SASS 0x20
 #endif /* NEARBY_FP_ENABLE_SASS */
 
-// Sets Fast Pair advertisement type
-nearby_platform_status nearby_fp_client_SetAdvertisement(int mode);
-
-// Initalizes Fast Pair provider. The |callbacks| are optional - can be NULL.
-nearby_platform_status nearby_fp_client_Init(
-    const nearby_fp_client_Callbacks* callbacks);
-
-#if NEARBY_FP_MESSAGE_STREAM
-// Serializes and sends |message| over Message Stream
-nearby_platform_status nearby_fp_client_SendMessage(
-    uint64_t peer_address, const nearby_message_stream_Message* message);
-
-// Sends out an ACK message for a received |message|. Note that not all messages
-// require an ACK
-nearby_platform_status nearby_fp_client_SendAck(
-    const nearby_event_MessageStreamReceived* message);
-
-// Sends out a NACK message for a received |message| with |fail_reason| reason.
-nearby_platform_status nearby_fp_client_SendNack(
-    const nearby_event_MessageStreamReceived* message, uint8_t fail_reason);
-
-// Sends out a enable or disable silence mode, which stops audio from being
-// routed to the provider.
-nearby_platform_status nearby_fp_client_SetSilenceMode(uint64_t peer_address,
-                                                       bool enable);
 
 // Sends log buffer full message to seeker, which passes it on to the companion
 // app to manage log collection.
