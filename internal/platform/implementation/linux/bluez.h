f9a62a24dca4ec5e9a1877f4fec4e72710738d4d
Vibhav Pant <vibhavp@gmail.com>
2023-07-23 19:01:50 +0530

    Add bluetooth_adapter implementation.

---
+++ internal/platform/implementation/linux/bluez.h
@@ -0,0 +1,13 @@
+#ifndef PLATFORM_IMPL_LINUX_BLUEZ_H_
+#define PLATFORM_IMPL_LINUX_BLUEZ_H_
+
+namespace nearby {
+namespace linux {
+const char *BLUEZ_SERVICE = "org.bluez";
+
+const char *BLUEZ_ADAPTER_INTERFACE = "org.bluez.Adapter1";
+
+} // namespace linux
+} // namespace nearby
+
+#endif
