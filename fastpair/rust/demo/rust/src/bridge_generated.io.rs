80a3b508fa9b518a464431186eb5e2daeaff1c16
Lucas Silva Shepard <theshepard@google.com>
2023-07-29 16:02:15 -0700

    [fp-rs] Providing constructs for generating Rust-side bridge code

---
+++ fastpair/rust/demo/rust/src/bridge_generated.io.rs
@@ -0,0 +1,36 @@
+use super::*;
+// Section: wire functions
+
+#[no_mangle]
+pub extern "C" fn wire_hello(port_: i64) {
+    wire_hello_impl(port_)
+}
+
+// Section: allocate functions
+
+// Section: related functions
+
+// Section: impl Wire2Api
+
+// Section: wire structs
+
+// Section: impl NewWithNullPtr
+
+pub trait NewWithNullPtr {
+    fn new_with_null_ptr() -> Self;
+}
+
+impl<T> NewWithNullPtr for *mut T {
+    fn new_with_null_ptr() -> Self {
+        std::ptr::null_mut()
+    }
+}
+
+// Section: sync execution mode utility
+
+#[no_mangle]
+pub extern "C" fn free_WireSyncReturn(ptr: support::WireSyncReturn) {
+    unsafe {
+        let _ = support::box_from_leak_ptr(ptr);
+    };
+}
