65f61333c0c71a27896d806b15dd65eea8084611
Vibhav Pant <vibhavp@gmail.com>
2023-08-23 16:01:57 +0530

    Add BluezObjectManager.

--- internal/platform/implementation/linux/bluez.h
+++ internal/platform/implementation/linux/bluez.h
@@ -1,6 +1,8 @@
 #ifndef PLATFORM_IMPL_LINUX_BLUEZ_H_
 #define PLATFORM_IMPL_LINUX_BLUEZ_H_
 
+#include <sdbus-c++/ProxyInterfaces.h>
+#include <sdbus-c++/StandardInterfaces.h>
 #include <sdbus-c++/Types.h>
 
 #include "absl/strings/string_view.h"
@@ -35,6 +37,25 @@ device_object_path(const sdbus::ObjectPath &adapter_object_path,
 extern sdbus::ObjectPath profile_object_path(absl::string_view service_uuid);
 
 extern sdbus::ObjectPath adapter_object_path(absl::string_view name);
+
+class BluezObjectManager
+    : public sdbus::ProxyInterfaces<sdbus::ObjectManager_proxy> {
+public:
+  BluezObjectManager(sdbus::IConnection &system_bus)
+      : ProxyInterfaces(system_bus, "org.bluez", "/") {
+    registerProxy();
+  }
+  ~BluezObjectManager() { unregisterProxy(); }
+
+protected:
+  void onInterfacesAdded(
+      const sdbus::ObjectPath &objectPath,
+      const std::map<std::string, std::map<std::string, sdbus::Variant>>
+          &interfacesAndProperties) override {}
+  void
+  onInterfacesRemoved(const sdbus::ObjectPath &objectPath,
+                      const std::vector<std::string> &interfaces) override {}
+};
 
 } // namespace bluez
 } // namespace linux
