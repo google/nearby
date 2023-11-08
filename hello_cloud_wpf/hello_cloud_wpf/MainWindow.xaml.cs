759aca62590e07413f2e58613f022ce4f818890a
deling-google <deling@google.com>
2023-11-07 22:33:11 -0800

    Skeleton of a WPF app for cloud transfer

---
+++ hello_cloud_wpf/hello_cloud_wpf/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hello_cloud_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IntPtr router = IntPtr.Zero;
+        IntPtr core = IntPtr.Zero;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.endpoints.Items.Add("Apple");
+            this.endpoints.Items.Add("Banana");
+            this.endpoints.Items.Add("Watermelon");
+            this.router = NearbyConnections.InitServiceControllerRouter();
+            if (this.router == IntPtr.Zero)
+            {
+                throw new Exception("Failed: InitServiceControllerRouter");
+            }
+
+            this.core = NearbyConnections.InitCore(this.router);
+            if (this.core == IntPtr.Zero)
+            {
+                throw new Exception("Failed: InitCore");
+            }
+
+            // Start Discovery
+        }
+
+        // End point discovered callback
+
+    }
+}
---
+++ hello_cloud_wpf/hello_cloud_wpf/NearbyConnections.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_cloud_wpf
+{
+    internal static class NearbyConnections
+    {
+        //DLL_API ServiceControllerRouter* __stdcall InitServiceControllerRouter();
+        [DllImport("nearby_connections_dart.dll")]
+        public static extern IntPtr InitServiceControllerRouter();
+
+        //DLL_API void __stdcall CloseServiceControllerRouter(ServiceControllerRouter*);
+        [DllImport("nearby_connections_dart.dll")]
+        public static extern IntPtr CloseServiceControllerRouter(IntPtr router);
+
+        //DLL_API Core* __stdcall InitCore(ServiceControllerRouter*);
+        [DllImport("nearby_connections_dart.dll")]
+        public static extern IntPtr InitCore(IntPtr serviceControllerRouter);
+
+        // Closes the core with stopping all endpoints, then free the memory.
+        //DLL_API void __stdcall CloseCore(Core*);
+        [DllImport("nearby_connections_dart.dll")]
+        public static extern void CloseCore(IntPtr core);
+    }
+}
