ba0697727df3199b845b22f1f55625fe6d0d9efe
deling-google <deling@google.com>
2023-11-11 12:26:13 -0800

    Skeleton code for hello_cloud_wpf

--- hello_cloud_wpf/hello_cloud_wpf/MainWindow.xaml.cs
+++ hello_cloud_wpf/hello_cloud_wpf/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,24 +16,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static hello_cloud_wpf.NearbyConnections;
 
 namespace hello_cloud_wpf
 {
+    public struct EndpointEntry
+    {
+        public string id;
+        public string name;
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         IntPtr router = IntPtr.Zero;
         IntPtr core = IntPtr.Zero;
 
+        public ObservableDictionary<string, string> Endpoints { get; }
+
         public MainWindow()
         {
+            Endpoints = new ObservableDictionary<string, string>();
             InitializeComponent();
 
-            this.endpoints.Items.Add("Apple");
-            this.endpoints.Items.Add("Banana");
-            this.endpoints.Items.Add("Watermelon");
+            AllocConsole();
+
             this.router = NearbyConnections.InitServiceControllerRouter();
             if (this.router == IntPtr.Zero)
             {
@@ -42,10 +59,94 @@ namespace hello_cloud_wpf
                 throw new Exception("Failed: InitCore");
             }
 
-            // Start Discovery
+            BooleanMediumSelector mediumSelector = new()
+            {
+                ble = true,
+                bluetooth = true,
+                web_rtc = true,
+                wifi_direct = true,
+                wifi_hotspot = true,
+                wifi_lan = true,
+            };
+
+            NearbyConnections.DiscoveryOptions discoveryOptions = new()
+            {
+                strategy = NearbyConnections.kP2pCluster,
+                allowed = mediumSelector,
+                auto_upgrade_bandwidth = true,
+                enforce_topology_constraints = true,
+                is_out_of_band_connection = false,
+                low_power = true,
+            };
+
+
+            EndpointFoundCallback endpointFoundCallback = this.OnEndpointFound;
+            EndpointLostCallback endpointLostCallback = this.OnEndpointLost;
+            EndpointDistanceChangedCallback endpointDistanceChangedCallback = this.OnEndpointDistanceChanged;
+            ResultCallback discoveryStartedCallback = this.OnDiscoveryStarted;
+
+            // Allocate memory these delegates so that they don't get GCed.
+            // TODO: free up the memory in the destructor.
+            GCHandle.Alloc(endpointFoundCallback);
+            GCHandle.Alloc(endpointLostCallback);
+            GCHandle.Alloc(endpointDistanceChangedCallback);
+            GCHandle.Alloc(discoveryStartedCallback);
+
+            NearbyConnections.StartDiscovery(
+                this.core,
+                "com.google.location.nearby.apps.helloconnections",
+                discoveryOptions,
+                endpointFoundCallback,
+                endpointLostCallback,
+                endpointDistanceChangedCallback,
+                discoveryStartedCallback);
         }
 
-        // End point discovered callback
+        void OnDiscoveryStarted(NearbyConnections.Status status)
+        {
+            System.Console.WriteLine("OnDiscoveryStarted: " + status.ToString());
+        }
 
+        void AddEndpointOnUIThread(string id, string name)
+        {
+            Action<string, string> addMethod = Endpoints.Add;
+            Application.Current.Dispatcher.BeginInvoke((string id, string name) => Endpoints.Add(id, name), id, name);
+        }
+
+        void RemoveEndpointOnUIThread(string id)
+        {
+            Application.Current.Dispatcher.BeginInvoke((string id) => Endpoints.Remove(id), id);
+        }
+
+        void OnEndpointFound(string endpointId, byte[] endpointInfo, int size, string serviceId)
+        {
+            var _ = size;
+            string endpointName = Encoding.UTF8.GetString(endpointInfo);
+            System.Console.WriteLine("OnEndPointFound: ");
+            System.Console.WriteLine("  endpoint_id: " + endpointId);
+            System.Console.WriteLine("  endpoint_info: " + endpointName);
+            System.Console.WriteLine("  service_id: " + serviceId);
+
+            AddEndpointOnUIThread(endpointId, endpointName);
+        }
+
+        void OnEndpointLost(string endpointId)
+        {
+            System.Console.WriteLine("OnEndpointLost: " + endpointId);
+            RemoveEndpointOnUIThread(endpointId);
+        }
+
+        void OnEndpointDistanceChanged(string endpointId, NearbyConnections.DistanceInfo distanceInfo)
+        {
+            System.Console.WriteLine("OnEndpointDistanceChanged: ");
+            System.Console.WriteLine("  endpoint_id: " + endpointId);
+            System.Console.WriteLine("  distance_info: " + distanceInfo);
+        }
+
+        void OnInitiated(string endpointId, NearbyConnections.ConnectionResponseInfo connectionResponseInfo)
+        {
+            System.Console.WriteLine("OnInitiated: ");
+            System.Console.WriteLine("  endpoint_id: " + endpointId);
+        }
     }
 }
--- hello_cloud_wpf/hello_cloud_wpf/NearbyConnections.cs
+++ hello_cloud_wpf/hello_cloud_wpf/NearbyConnections.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -7,23 +8,184 @@ using System.Threading.Tasks;
 
 namespace hello_cloud_wpf
 {
-    internal static class NearbyConnections
+    public static class NearbyConnections
     {
+        const string nearby_connections_dll = "nearby_connections_dart.dll";
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Strategy
+        {
+            public enum ConnectionType
+            {
+                kNone = 0,
+                kPointToPoint = 1,
+            };
+
+            public enum TopologyType
+            {
+                kUnknown = 0,
+                kOneToOne = 1,
+                kOneToMany = 2,
+                kManyToMany = 3,
+            };
+
+            public ConnectionType connection_type;
+            public TopologyType topology_type;
+        };
+
+        public static Strategy kP2pCluster = new()
+        {
+            connection_type = Strategy.ConnectionType.kPointToPoint,
+            topology_type = Strategy.TopologyType.kManyToMany
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BooleanMediumSelector
+        {
+            [MarshalAs(UnmanagedType.U1)] public bool bluetooth;
+            [MarshalAs(UnmanagedType.U1)] public bool ble;
+            [MarshalAs(UnmanagedType.U1)] public bool web_rtc;
+            [MarshalAs(UnmanagedType.U1)] public bool wifi_lan;
+            [MarshalAs(UnmanagedType.U1)] public bool wifi_hotspot;
+            [MarshalAs(UnmanagedType.U1)] public bool wifi_direct;
+        };
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct DiscoveryOptions
+        {
+            [FieldOffset(0)] public Strategy strategy;
+            [FieldOffset(8)] public BooleanMediumSelector allowed;
+            [FieldOffset(16)][MarshalAs(UnmanagedType.U1)] public bool auto_upgrade_bandwidth;
+            [FieldOffset(17)][MarshalAs(UnmanagedType.U1)] public bool enforce_topology_constraints;
+            [FieldOffset(18)][MarshalAs(UnmanagedType.U1)] public bool is_out_of_band_connection;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            [FieldOffset(24)] public char[] unused; // std::string fast_advertisement_service_uuid;
+            [FieldOffset(56)][MarshalAs(UnmanagedType.U1)] public bool low_power;
+        };
+
+        public enum Status
+        {
+            kSuccess,
+            kError,
+            kOutOfOrderApiCall,
+            kAlreadyHaveActiveStrategy,
+            kAlreadyAdvertising,
+            kAlreadyDiscovering,
+            kAlreadyListening,
+            kEndpointIoError,
+            kEndpointUnknown,
+            kConnectionRejected,
+            kAlreadyConnectedToEndpoint,
+            kNotConnectedToEndpoint,
+            kBluetoothError,
+            kBleError,
+            kWifiLanError,
+            kPayloadUnknown,
+            kReset,
+            kTimeout,
+            kUnknown,
+            kNextValue,
+        };
+
+        public enum DistanceInfo
+        {
+            kUnknown = 1,
+            kVeryClose = 2,
+            kClose = 3,
+            kFar = 4,
+        };
+
+        public enum Medium
+        {
+            kUnknown = 0,
+            kMdns = 1,
+            kBluetooth = 2,
+            kWiFiHotSpot = 3,
+            kBle = 4,
+            kWiFiLan = 5,
+            kWifiAware = 6,
+            kNfc = 7,
+            kWiFiDirect = 8,
+            kWebRtc = 9,
+            kBleL2Cap = 10,
+            kUsb = 11,
+        };
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct AdvertisingOptions
+        {
+            [FieldOffset(0)] public Strategy strategy;
+            [FieldOffset(8)] public BooleanMediumSelector allowed;
+            [FieldOffset(16)][MarshalAs(UnmanagedType.U1)] public bool auto_upgrade_bandwidth;
+            [FieldOffset(17)][MarshalAs(UnmanagedType.U1)] public bool enforce_topology_constraints;
+            [FieldOffset(18)][MarshalAs(UnmanagedType.U1)] public bool low_power;
+            [FieldOffset(19)][MarshalAs(UnmanagedType.U1)] public bool enable_bluetooth_listening;
+            [FieldOffset(20)][MarshalAs(UnmanagedType.U1)] public bool enable_webrtc_listening;
+            [FieldOffset(21)][MarshalAs(UnmanagedType.U1)] public bool is_out_of_band_connection;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            [FieldOffset(24)] public char[] unused; // std::string fast_advertisement_service_uuid;
+            [FieldOffset(56)] string device_info;
+        };
+
+        // TODO: use proto
+        public struct ConnectionResponseInfo
+        {
+            public byte[] endpointInfo;
+            public string authenticationToken; // always 4 characters
+            public string rawAuthenticationToken;
+            public bool isIncomingConnection;
+            public bool isConnectionVerified;
+        };
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void ResultCallback(Status status);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void EndpointFoundCallback(string endpointId,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] byte[] endpointInfo, int size, string serviceId);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void EndpointLostCallback(string endpointId);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void EndpointDistanceChangedCallback(string endpointId, DistanceInfo distanceInfo);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void InitiatedCallback(string endpointId, ConnectionResponseInfo connectionResponseInfo);
+
         //DLL_API ServiceControllerRouter* __stdcall InitServiceControllerRouter();
-        [DllImport("nearby_connections_dart.dll")]
+        [DllImport(nearby_connections_dll)]
         public static extern IntPtr InitServiceControllerRouter();
 
         //DLL_API void __stdcall CloseServiceControllerRouter(ServiceControllerRouter*);
-        [DllImport("nearby_connections_dart.dll")]
+        [DllImport(nearby_connections_dll)]
         public static extern IntPtr CloseServiceControllerRouter(IntPtr router);
 
         //DLL_API Core* __stdcall InitCore(ServiceControllerRouter*);
-        [DllImport("nearby_connections_dart.dll")]
+        [DllImport(nearby_connections_dll)]
         public static extern IntPtr InitCore(IntPtr serviceControllerRouter);
 
-        // Closes the core with stopping all endpoints, then free the memory.
         //DLL_API void __stdcall CloseCore(Core*);
-        [DllImport("nearby_connections_dart.dll")]
+        [DllImport(nearby_connections_dll)]
         public static extern void CloseCore(IntPtr core);
+
+        [DllImport(nearby_connections_dll, EntryPoint = "StartDiscoverySharp")]
+        public static extern void StartDiscovery(
+            IntPtr core,
+            [MarshalAs(UnmanagedType.LPStr)] string service_id,
+            DiscoveryOptions discovery_options,
+            EndpointFoundCallback endpoint_found_callback,
+            EndpointLostCallback endpoint_lost_callback,
+            EndpointDistanceChangedCallback endpoint_distance_changed_callback,
+            ResultCallback start_discovery_callback);
+
+        [DllImport(nearby_connections_dll, CharSet = CharSet.Unicode, EntryPoint = "StartAdvertisingSharp")]
+        public static extern void StartAdvertising(
+            IntPtr pCore,
+            [MarshalAs(UnmanagedType.LPStr)] string service_id,
+            AdvertisingOptions advertising_options,
+            byte[] endpoint_info,
+            InitiatedCallback initiated_callback,
+            ResultCallback start_advertising_callback);
     }
 }
