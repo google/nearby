76deda4ab94c2ff119c846d4588577b71569e7c3
deling-google <deling@google.com>
2023-11-22 17:02:45 -0800

    WPF client

---
+++ hello_cloud_wpf/hello_cloud_wpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace HelloCloudWpf {
+    public struct EndpointEntry
+    {
+        public string id;
+        public string name;
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        public MainWindow()
+        {
+            //AllocConsole();
+            InitializeComponent();
+        }
+
+        private void IsAdvertisingChecked(object sender, RoutedEventArgs e) {
+            (DataContext as MainViewModel)?.StartAdvertising();
+        }
+
+        private void IsAdvertisingUnchecked(object sender, RoutedEventArgs e) {
+            (DataContext as MainViewModel)?.StopAdvertising();
+        }
+
+        private void IsDiscoveringChecked(object sender, RoutedEventArgs e) {
+            (DataContext as MainViewModel)?.StartDiscovering();
+        }
+
+        private void IsDiscoveringUnchecked(object sender, RoutedEventArgs e) {
+            (DataContext as MainViewModel)?.StopDiscovering();
+
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e) {
+            (DataContext as MainViewModel)?.Deinit();
+        }
+    }
+}
