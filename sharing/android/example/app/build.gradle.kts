223ff133b823b001796f3f62ee95ea132dd3c245
Anay Wadhera <awadhera@google.com>
2024-01-10 18:46:42 -0800

    Remove no-op proguard rules file

--- sharing/android/example/app/build.gradle.kts
+++ sharing/android/example/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
   buildTypes {
     release {
       isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
     }
   }
   compileOptions {
