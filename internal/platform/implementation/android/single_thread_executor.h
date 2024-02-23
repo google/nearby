b0d22bb62309c198aa96fca7d4ded905332e68da
Guogang Li <guogang@google.com>
2024-02-23 15:40:27 -0800

    Added API declarations for Android platform
    PiperOrigin-RevId: 609856310

---
+++ internal/platform/implementation/android/single_thread_executor.h
@@ -0,0 +1,34 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PLATFORM_IMPL_ANDROID_SINGLE_THREAD_EXECUTOR_H_
+#define PLATFORM_IMPL_ANDROID_SINGLE_THREAD_EXECUTOR_H_
+
+#include "internal/platform/implementation/android/multi_thread_executor.h"
+
+namespace nearby {
+namespace android {
+
+// An Executor that uses a single worker thread operating off an unbounded
+// queue.
+class SingleThreadExecutor final : public MultiThreadExecutor {
+ public:
+  SingleThreadExecutor() : MultiThreadExecutor(1) {}
+  ~SingleThreadExecutor() override = default;
+};
+
+}  // namespace android
+}  // namespace nearby
+
+#endif  // PLATFORM_IMPL_ANDROID_SINGLE_THREAD_EXECUTOR_H_
