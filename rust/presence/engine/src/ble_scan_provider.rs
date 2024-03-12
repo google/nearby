ebf4f39930050175e39d750f36b26c9798e4c1bd
Chen Wang <chenw@google.com>
2024-03-12 06:41:54 +0000

    Initial Presence Engine rust implementation.
    export RUST_LOG=info && cargo run

--- rust/presence/engine/src/ble_scan_provider.rs
+++ rust/presence/engine/src/ble_scan_provider.rs
@@ -2,9 +2,16 @@ use std::sync::mpsc::Receiver;
 use log::{error, info};
 use tokio::sync::mpsc;
 use crate::fused_engine::ProviderEvent;
-use crate::ble_scan_emulator;
-use crate::ble_scan_emulator::register_provider_tx;
-use crate::scan_controller::ScanControlMsg;
+use crate::{ble_scan_emulator, provider};
+use crate::scan_controller::{ScanControlMsg, ScanData, ScanDataMsg};
+
+static mut PROVIDER_TX: Option<mpsc::Sender<ProviderEvent>> = None;
+pub fn on_scan_data(scan_data: ScanData) {
+    unsafe {
+        provider::update_engine(PROVIDER_TX.as_ref().unwrap().clone(),
+                                ProviderEvent::ScanDataMsg(ScanDataMsg::ScanData(scan_data)));
+    }
+}
 
 pub struct BleScanProvider {
     // Used by FusedEngine to send commands to the Provider.
@@ -12,22 +19,21 @@ pub struct BleScanProvider {
 }
 
 impl BleScanProvider {
-    pub fn new(provider_event_tx: mpsc::Sender<ProviderEvent>, rx: mpsc::Receiver<ScanControlMsg>) -> Self {
-        register_provider_tx(provider_event_tx);
+    pub fn new(provider_event_tx: mpsc::Sender<ProviderEvent>,
+               rx: mpsc::Receiver<ScanControlMsg>) -> Self {
+        unsafe {
+            PROVIDER_TX = Some(provider_event_tx);
+        }
         Self {
             engine_rx: rx,
         }
     }
 
-    pub fn set_channels(&mut self, tx: mpsc::Sender<ProviderEvent>) {
-        register_provider_tx(tx.clone());
-    }
-
     pub async fn run(&mut self) {
         info!("BLE scan Provider starts.");
         loop {
-            if let Some(i) = self.engine_rx.recv().await {
-                info!("BLE scan provider receives {:?}", i);
+            if let Some(scan_control_msg) = self.engine_rx.recv().await {
+                info!("BLE scan provider receives {:?}", scan_control_msg);
                 ble_scan_emulator::start_scan(ble_scan_emulator::BleScanRequest{});
             } else {
                 error!("BLE scan Provider failed to receive a command from the fused Engine.");
