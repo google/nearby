832d267e26a29f5c273a22401d411a245ddf6db8
Anay Wadhera <awadhera@google.com>
2024-08-25 17:30:23 -0700

    Switch to codegen for Nearby Connections flags.
    PiperOrigin-RevId: 667396816

--- connections/implementation/flags/nearby_connections_feature_flags.h
+++ connections/implementation/flags/nearby_connections_feature_flags.h
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Mendel flags, auto-generated. DO NOT EDIT.
 #ifndef THIRD_PARTY_NEARBY_CONNECTIONS_IMPLEMENTATION_FLAGS_NEARBY_CONNECTIONS_FEATURE_FLAGS_H_
 #define THIRD_PARTY_NEARBY_CONNECTIONS_IMPLEMENTATION_FLAGS_NEARBY_CONNECTIONS_FEATURE_FLAGS_H_
 
@@ -26,61 +27,47 @@ namespace config_package_nearby {
 
 constexpr absl::string_view kConfigPackage = "nearby";
 
-// The Nearby Connections features.
 namespace nearby_connections_feature {
-
-// Disable/Enable BLE v2 in Nearby Connections SDK.
-constexpr auto kEnableBleV2 =
-    flags::Flag<bool>(kConfigPackage, "45401515", false);
-
 // The timeout in millis to report peripheral device lost.
 constexpr auto kBlePeripheralLostTimeoutMillis =
     flags::Flag<int64_t>(kConfigPackage, "45411439", 12000);
-
-// Enable/Disable GATT query during scanning.
-constexpr auto kEnableGattQueryInThread =
-    flags::Flag<bool>(kConfigPackage, "45415261", false);
-
-// Enable/Disable payload manager to skip chunk update.
-constexpr auto kEnablePayloadManagerToSkipChunkUpdate =
-    flags::Flag<bool>(kConfigPackage, "45415729", false);
-
-// Enable/Disable safe-to-disconnect feature.
-constexpr auto kEnableSafeToDisconnect =
-    flags::Flag<bool>(kConfigPackage, "45425789", false);
-
+// When true, disable Bluetooth classic scanning.
+constexpr auto kDisableBluetoothClassicScanning =
+    flags::Flag<bool>(kConfigPackage, "45639961", false);
 // Enable/Disable auto_reconnect feature.
 constexpr auto kEnableAutoReconnect =
     flags::Flag<bool>(kConfigPackage, "45427690", false);
-
-// When true, allows to enable payload-received-ack protocol.
-constexpr auto kEnablePayloadReceivedAck =
-    flags::Flag<bool>(kConfigPackage, "45425840", false);
-
-// Support 0. disabled all. 1. safe-to-disconnect 2. reserved 3. auto-reconnect
-// 4. auto-resume for dev device 5. payload_ack
-constexpr auto kSafeToDisconnectVersion =
-    flags::Flag<int64_t>(kConfigPackage, "45425841", 0);
-
-// When true, allows to enable Multiplex feature.
-constexpr auto kEnableMultiplex =
-    flags::Flag<bool>(kConfigPackage, "45627836", false);
-
-// When true, use the medium in connection info.
-constexpr auto kUseStableEndpointId =
-    flags::Flag<bool>(kConfigPackage, "45639298", false);
-
-// When true, use the medium in connection info.
-constexpr auto kDisableBluetoothClassicScanning =
-    flags::Flag<bool>(kConfigPackage, "45639961", false);
-
-// When true, enable instant on lost feature
+// Disable/Enable BLE v2 in Nearby Connections SDK.
+constexpr auto kEnableBleV2 =
+    flags::Flag<bool>(kConfigPackage, "45401515", false);
+// Disable/Enable GATT query in thread in BLE V2.
+// Manual edit: setting this to false for ChromeOS rollout as well.
+constexpr auto kEnableGattQueryInThread =
+    flags::Flag<bool>(kConfigPackage, "45415261", false);
+// When true, enable instant on lost feature.
 constexpr auto kEnableInstantOnLost =
     flags::Flag<bool>(kConfigPackage, "45642180", false);
-
-// When true, enable Wi-Fi Hotspot client.
+// When true, enable multiplexing in NC.
+constexpr auto kEnableMultiplex =
+    flags::Flag<bool>(kConfigPackage, "45647946", false);
+// Enable/Disable payload manager to skip chunk update.
+constexpr auto kEnablePayloadManagerToSkipChunkUpdate =
+    flags::Flag<bool>(kConfigPackage, "45415729", true);
+// Enable/Disable payload-received-ack feature.
+constexpr auto kEnablePayloadReceivedAck =
+    flags::Flag<bool>(kConfigPackage, "45425840", false);
+// Enable/Disable safe-to-disconnect feature.
+constexpr auto kEnableSafeToDisconnect =
+    flags::Flag<bool>(kConfigPackage, "45425789", false);
+// by default, enable Wi-Fi Hotspot client.
 constexpr auto kEnableWifiHotspotClient =
     flags::Flag<bool>(kConfigPackage, "45648734", true);
+// Enable/Disable payload-received-ack feature.
+constexpr auto kSafeToDisconnectVersion =
+    flags::Flag<int64_t>(kConfigPackage, "45425841", 0);
+// When true, use stable endpoint ID.
+constexpr auto kUseStableEndpointId =
+    flags::Flag<bool>(kConfigPackage, "45639298", false);
 
 }  // namespace nearby_connections_feature
 }  // namespace config_package_nearby
