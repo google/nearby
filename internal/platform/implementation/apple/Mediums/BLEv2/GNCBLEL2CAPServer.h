97d60b8babf974dde909959971e3fe7cbfdf5ac8
Edwin Wu <edwinwu@google.com>
2025-03-30 01:48:06 -0700

    Implement openL2CAPServerWithCompletionHandler in GNCBLEMedium class.
    PiperOrigin-RevId: 741972788

--- internal/platform/implementation/apple/Mediums/BLEv2/GNCBLEL2CAPServer.h
+++ internal/platform/implementation/apple/Mediums/BLEv2/GNCBLEL2CAPServer.h
@@ -15,7 +15,16 @@
 #import <CoreBluetooth/CoreBluetooth.h>
 #import <Foundation/Foundation.h>
 
+@protocol GNCPeripheralManager;
+
 NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * Completion handler for starting listening for an L2CAP channel.
+ *
+ * @param error The cause of the failure, or @c nil if no error occurred.
+ */
+typedef void (^GNCStartListeningL2CAPChannelCompletionHandler)(NSError *_Nullable error);
 
 /**
  * An object that publishes the @c PSM value.
@@ -28,11 +37,25 @@ NS_ASSUME_NONNULL_BEGIN
 @property(atomic, readonly) CBL2CAPPSM PSM;
 
 /**
- * Retrieves the L2CAP channel.
+ * Creates a L2CAP server with a provided peripheral manager.
  *
- * @return The L2CAP channel, or nil if the channel is not available.
+ * @param peripheralManager The peripheral manager instance. Note: This is only exposed for testing
+ *                          and can be used to inject a fake peripheral manager.
  */
-- (nullable CBL2CAPChannel *)getChannel;
+- (instancetype)initWithPeripheralManager:(nullable id<GNCPeripheralManager>)peripheralManager;
+
+/**
+ * Starts listening for an L2CAP channel.
+ *
+ * @param completionHandler The completion handler to call when the L2CAP channel is started.
+ */
+- (void)startListeningChannelWithCompletionHandler:
+    (nullable GNCStartListeningL2CAPChannelCompletionHandler)completionHandler;
+
+/**
+ * Closes the L2CAP channel
+ */
+- (void)close;
 
 @end
 
