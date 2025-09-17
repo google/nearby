3f4e2a6ca04e91f7ceae757854538690a723858b
Francis Tsui <ftsui@google.com>
2025-09-17 10:55:46 -0700

    Add flag to enable async grpc client.
    PiperOrigin-RevId: 808220477

--- sharing/flags/generated/nearby_sharing_feature_flags.h
+++ sharing/flags/generated/nearby_sharing_feature_flags.h
@@ -76,6 +76,9 @@ constexpr auto kDeleteUnexpectedReceivedFileFix =
 // The default time in milliseconds a cached entry can be in LOST state.
 constexpr auto kDiscoveryCacheLostExpiryMs =
     flags::Flag<int64_t>(kConfigPackage, "45658774", 500);
+// When true, enable Nearby Share gRPC async client.
+constexpr auto kEnableNearbyShareGrpcAsyncClient =
+    flags::Flag<bool>(kConfigPackage, "45726584", false);
 // When true, enable wifi hotspot medium for HP Realtek devices.
 constexpr auto kEnableWifiHotspotForHpRealtekDevices =
     flags::Flag<bool>(kConfigPackage, "45673628", false);
@@ -128,6 +131,7 @@ inline absl::btree_map<int, const flags::Flag<bool>&> GetBoolFlags() {
       {45409033, kShowAutoUpdateSetting},
       {45630055, kUseGrpcClient},
       {45657036, kDeleteUnexpectedReceivedFileFix},
+      {45726584, kEnableNearbyShareGrpcAsyncClient},
       {45673628, kEnableWifiHotspotForHpRealtekDevices},
       {45683539, kUseAlternateServiceUuidForDiscovery},
       {45417647, kEnableQrCodeUi},
